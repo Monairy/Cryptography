0249c33fe92bf5dbca719c0615c1b51f0f7532ad
Ahmed EL-Monairy <42189296+Monairy@users.noreply.github.com>
2021-01-26 15:57:32 +0200

    Update AES.py

--- AES.py
+++ AES.py
@@ -24,32 +24,80 @@ S_BOX = [
             0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0,
             0x54, 0xbb, 0x16]
 
+inv_S_BOX = [
+           0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+           0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+           0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+           0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+           0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+           0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+           0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+           0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+           0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+           0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+           0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+           0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+           0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+           0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+           0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+           0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d]
+
+
 Mix_Col = [
            [2,3,1,1],
            [1,2,3,1],
            [1,1,2,3],
            [3,1,1,2]]
 
+invMix_Col=[
+           [0x0E, 0x0B, 0x0D, 0x09],
+           [0x09, 0x0E, 0x0B, 0x0D],
+           [0x0D, 0x09, 0x0E, 0x0B],
+           [0x0B, 0x0D, 0x09, 0x0E]]
+
 R_CON=[0x1,0x2,0x4,0x8,0x10,0x20,0x40,0x80,0x1b,0x36]
+
+
+def SBOX(val):
+    index=int(val[0],16)*16 + int(val[1],16)
+    return '{:02x}'.format(S_BOX[index])
+     
+def invSBOX(val):
+    index=int(val[0],16)*16 + int(val[1],16)
+    return '{:02x}'.format(inv_S_BOX[index])
 
 def subBytes(matrix):
    out=[[],[],[],[]]
    for row in range(0,4):
     for col in range(0,4):
-        cell=matrix[row][col]
-        index=int(cell[0],16)*16 + int(cell[1],16)
-        out[row].append('{:02x}'.format(S_BOX[index]))
+        val=matrix[row][col]
+        out[row].append(SBOX(val))
+   return out
+
+def invsubBytes(matrix):
+   out=[[],[],[],[]]
+   for row in range(0,4):
+    for col in range(0,4):
+        val=matrix[row][col]
+        out[row].append(invSBOX(val))
    return out
 
 def shiftRows(matrix):
   out=[[],[],[],[]]
   shamt=0
   for row in range(0,4):
-      out[row]=matrix[row][shamt:]+matrix[row][0:shamt]
+      out[row]=matrix[row][shamt:]+matrix[row][:shamt]
       shamt+=1
-
   return out
 
+
+def invshiftRows(matrix):
+  out=[[],[],[],[]]
+  shamt=0
+  for row in range(0,4):
+      out[row]=matrix[row][4-shamt:]+matrix[row][:4-shamt]
+      shamt+=1
+  return out
 
 def mixColumns(matrix):
     out=[[],[],[],[]]
@@ -82,6 +130,58 @@ def mixColumns(matrix):
                     temp^=currentCol[col_mc]  #add self
                     cell ^= temp
                     
+            out[row_mc].append('{:02x}'.format(cell))            
+    return out
+
+def checkoverflow(num):
+    if(num> 0xff):
+      num &= 0xff  #make temp 8 bits only
+      num = num ^ 0x1b  #xor with field polyn
+    return num
+def shift(num,shamt):
+           
+   for i in range(0,shamt):
+      num=num<<1
+      num=checkoverflow(num)
+   return num
+
+def galoisMult(a, b):
+  out=0
+ #5 print(a,b)
+  a=str(bin(a)[2:].zfill(8))
+  shamt=1   
+  for i in range(len(a)-2,-1,-1):
+      if(a[i]=='1'):
+           out = out ^ shift(b,shamt)
+      shamt+=1
+      
+  if(a[len(a)-1]=='1'):
+     out = out ^ b
+  #print(a,b,out)   
+  return out
+  
+#a=galoisMult(14,49)^galoisMult(11,162)^galoisMult(13,64)^galoisMult(9,203)
+#print(a)
+
+#a^=galoisMult(3,137)
+#a^=galoisMult(1,171)
+#a^=galoisMult(1,205)
+
+def invmixColumns(matrix):
+    out=[[],[],[],[]]
+    for col_ip in range(0,4):
+               
+        currentCol=[]
+        for i in range(0,4): currentCol.append(int(matrix[i][col_ip],16))
+        
+        for row_mc in range(0,4): #row mix_col
+            cell=0
+            
+            for col_mc in range(0,4): #col mix_col
+               #print(invMix_Col[row_mc][col_mc] , currentCol[col_mc])      
+               cell^=galoisMult(currentCol[col_mc],invMix_Col[row_mc][col_mc])
+
+            #print(cell)       
             out[row_mc].append('{:02x}'.format(cell))
             
     return out
@@ -127,8 +227,7 @@ def keyExpansion(key,rnd):
         wi_1=wi_1[1:]+wi_1[:1]  #rot
         for i in range(0,4): #sBOX
           val=wi_1[i]
-          index=int(val[0],16)*16 + int(val[1],16)
-          wi_1[i]='{:02x}'.format(S_BOX[index])
+          wi_1[i]=SBOX(val)
       
       w=list()
       for i in range(0,4):
@@ -155,6 +254,33 @@ def encrypt(plainMatrix,keyMatrix):
 
    print(cipher)
 
+
+def decrypt(cipherMatrix,keyMatrix):
+   
+   allkeys=list()
+   allkeys.append(keyMatrix)
+   
+   for rnd in range(1,11):
+         keyMatrix= keyExpansion(keyMatrix,rnd)
+         #print(keyMatrix,"\n")
+         allkeys.append(keyMatrix)
+
+   plain=addRoundKey(cipherMatrix,allkeys[10])
+   print(plain)
+   
+
+   for rnd in range(1,11):
+      keyMatrix=allkeys[10-rnd]
+      if (rnd<10):
+          plain=invshiftRows(plain)
+          plain=addRoundKey(invsubBytes(plain),keyMatrix)
+          plain = invmixColumns(plain)
+
+      else:
+          plain=addRoundKey(invsubBytes(invshiftRows(plain)),allkeys[0])
+
+   print(plain)
+   
 arr=[['01','89','fe','76'],
      ['23','ab','dc','54'],
      ['45','cd','ba','32'],
@@ -165,7 +291,14 @@ roundKey=[['0f','47','0c','af'],
           ['71','e8','ad','67'],
           ['c9','59','d6','98']]
 
-encrypt(arr,roundKey)
+#encrypt(arr,roundKey)
+
+cipher=[['ff', '08', '69', '64'],
+        ['0b', '53', '34', '14'],
+        ['84', 'bf', 'ab', '8f'],
+        ['4a', '7c', '43', 'b9']]
+
+decrypt(cipher,roundKey)
 #keyExpansion(roundKey,2)
 
 #print(addRoundKey(mixColumns(shiftRows(subBytes(arr))),roundKey))
